05eee615a3a60a7449b92da3362a3caa7b8ddfaa
violetdiamonds <violetdiamonds@protonmail.com>
2026-03-02 16:47:31 -0500

    example 1

---
+++ package_exploration/play-by-play.py
@@ -0,0 +1,10 @@
+"""This is an example of how to use play-by-play data"""
+import nfl_data_py as nfl
+
+# Get play-by-play object
+# Returns a pandas dataframe
+pbp = nfl.import_pbp_data([2025])
+
+
+
+x = 1

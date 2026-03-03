679024da0c2301c5182e24e6159045708a63cc3e
violetdiamonds <violetdiamonds@protonmail.com>
2026-03-03 14:36:40 -0500

    epa dashboard

--- examples/epa_dashboard.py
+++ examples/epa_dashboard.py
@@ -2,6 +2,7 @@ import nfl_data_py as nfl
 import pandas as pd
 import numpy as np
 
+from utilities import yards_to_go_bucket
 
 # Load 2025 play-by-play data
 pbp = nfl.import_pbp_data([2025])
@@ -12,3 +13,17 @@ pbp = pbp[
      (pbp['epa'].notna()))
 ].copy()
 
+# Yards to go bucket
+pbp['yards_to_go_bucket'] = pbp['ydstogo'].apply(yards_to_go_bucket)
+
+# League average epa by play type, down, and yards to go
+league_avg_epa = (
+    pbp
+    .groupby(['play_type', 'down', 'yards_to_go_bucket'])
+    .agg(
+        league_epa=('epa', 'mean'),
+    )
+    .reset_index()
+)
+
+x = 1
